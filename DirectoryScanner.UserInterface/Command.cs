380841a6fcdf844e2d19430dfcacfaf579116347
Artsem Kulik <artsem.kulik@gmail.com>
2023-01-01 23:36:26 +0300

    Added Command

---
+++ DirectoryScanner.UserInterface/Command.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DirectoryScanner.UserInterface
+{
+    public class Command : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public Command(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object param)
+        {
+            return _canExecute(param);
+        }
+
+        public void Execute(object param)
+        {
+            _execute(param);
+        }
+    }
+}
